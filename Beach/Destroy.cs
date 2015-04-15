e22b5fa3de763270da0da2f6caa0f00c7257d5f5
Sourabh <sourabh.jamalapuram@gmail.com>
2015-04-14 20:47:21 -0700

    Adding Beach Destroy Script

---
+++ Beach/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+	public AudioClip trash;
+	
+	void OnTriggerEnter (Collider co)
+	{
+		print ("ontriggerenter :" + co.tag);
+		print ("ontriggerenter gameobject:" + co.gameObject);
+
+		if (co.tag.StartsWith ("Trash") || co.tag.StartsWith ("Bottle")) {
+			Destroy (co.gameObject);
+			print ("destroyed");
+			var audio = GetComponent<AudioSource>();
+
+			audio.PlayOneShot(trash,20);
+
+			var scoreComponent = GetComponent<ScoreComponent> ();
+			scoreComponent.ScoreChangeOnCollision();
+		}
+
+	
+	}
+}
