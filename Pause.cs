1791432e577d43c4d802ecb107c17155ddadd111
Neel Balusa <neelbalusa@gmail.com>
2015-04-15 20:39:24 -0700

    added pause

---
+++ Pause.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	bool isPaused;
+	string name = "Pause";
+	void OnGUI()
+	{
+
+		if(GUI.Button(new Rect(680,25,100,50),name))
+		   {
+			if(isPaused == true)
+			{
+				name = "Pause";
+				Time.timeScale = 1.0f;
+				isPaused = false;	
+			}
+			else
+			{
+			name = "Play";
+			Time.timeScale = 0.0f;
+			isPaused = true;
+			}
+			}
+			
+
+	}
+}
+
